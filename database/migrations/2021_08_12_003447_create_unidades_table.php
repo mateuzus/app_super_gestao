adee851b01db66e5632558b8156ca985a5e62cdc
Mateus Marcelino <mateusdsmarcelino@gmail.com>
2021-08-11 22:00:34 -0300

    relationship of unit tables with product details and products, 1-to-many relationship.

---
+++ database/migrations/2021_08_12_003447_create_unidades_table.php
@@ -0,0 +1,58 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnidadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unidades', function (Blueprint $table) {
+            $table->id();
+            $table->string('unit', 5);
+            $table->string('description', 30);
+            $table->timestamps();
+        });
+        // relacionamento com a tabela produtos
+        Schema::table('produtos', function (Blueprint $table){
+            $table->unsignedBigInteger('unit_id');
+            $table->foreign('unit_id')->references('id')->on('unidades');
+        });
+
+        // relacionamento com a tabela produtos detalhes
+        Schema::table('produto_detalhes', function (Blueprint $table){
+            $table->unsignedBigInteger('unit_id');
+            $table->foreign('unit_id')->references('id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //remover relacionamento com a tabela produtos produtos
+        Schema::table('produto_detalhes', function (Blueprint $table){
+            //remover a foreign key
+            $table->dropForeign('produto_detalhes_unit_id_foreign'); //tabela_coluna_foreign
+            //remover coluna unit_id
+            $table->dropColumn('unit_id');
+        });
+        // remover relacionamento com a tabela produtos detalhes
+        //remover foreign key
+        Schema::table('produtos', function (Blueprint $table){
+            $table->dropForeign('produto_unit_id_foreign');
+            //remover coluna unit_id
+            $table->dropColumn('unit_id');
+        });
+        Schema::dropIfExists('unidades');
+    }
+}
